e292a7559196c88a808b9935542af4245625840f
Carlo <carlo.poggi98@gmail.com>
2022-07-13 18:54:20 +0200

    add task-item

--- components/task-item.js
+++ components/task-item.js
@@ -1,9 +1,13 @@
 function renderElement() {
  let testo = document.getElementById("text-box").value
 
- return `<div>  
-            ${testo}
-        </div>
-          ` 
-           
- }
+ return `
+  <div>
+    <div>   
+      <input type="checkbox" id="fatto" name="fatto">
+      <label for="fatto">Fatto?</label>
+    </div>  
+    <p> ${testo} </p>
+  </div>
+` 
+}
