baae54883dcd5887489f06498b19a61b3956333d
Carlo <carlo.poggi98@gmail.com>
2022-07-14 13:14:55 +0200

    update store + add methods

--- store.js
+++ store.js
@@ -1,6 +1,41 @@
-
 const store = {
-  tasks: {
-
-  }
+  todos: {},
+  done: {}
 }
+
+
+function addTask() {
+  var text = document.getElementById('text-box').value
+  var id = Math.random()
+  store.todos[id] = text
+  renderList()
+}
+
+function getTodoTask(id) {
+  return store.todos[id] || {}
+}
+
+function getCompletedTask(id) {
+  return store.done[id] || {}
+}
+
+function deleteTodo(id) {
+  delete store.todos[id]
+  renderList()
+}
+
+function deleteDone(id) {
+  delete store.done[id]
+  renderList()
+}
+
+function toggleCompletedTask(id) {
+  if (store.done[id]) {
+    store.todos[id] = store.done[id]
+    deleteTask('todos', id)
+    return
+  }
+  store.done[id] = store.todos[id]
+  deleteTask('done', id)
+}
+
